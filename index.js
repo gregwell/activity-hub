eb29cc01a6a726b237f7cd689f84d5ca20c092a9
DESKTOP-27CQTIN\grzegurz <grzegorzstudzinski@hotmail.com>
2021-01-13 12:22:52 +0100

    init fixes

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-//import express from 'express'; 
-//import bodyParser from 'body-parser';
-
-const express = require('express');
-const bodyParser = require('body-parser');
+import express from 'express';
+import bodyParser from 'body-parser';
 const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
 
-app.listen(PORT, () => console.log(`Server running on port: http://localhost${PORT}`));
+app.listen(PORT, () => console.log(`Server Running on port: http://localhost${PORT}`));
+
