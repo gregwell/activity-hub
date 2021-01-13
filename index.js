4ae7679ca035e14a29336fb9996325f486b0b8ae
DESKTOP-27CQTIN\grzegurz <grzegorzstudzinski@hotmail.com>
2021-01-13 20:48:08 +0100

    CRUD routing implementation

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+
+import usersRoutes from './routes/users.js';
+
 const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
 
-app.listen(PORT, () => console.log(`Server Running on port: http://localhost${PORT}`));
+app.use('/users', usersRoutes);
+
+app.get('/',(req,res) => res.send('Hello from homepage'));
+
+app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`));
 
---
+++ routes/users.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+
+const router = express.Router();
+
+import { getUsers, createUser, deleteUser, getUserById, patchUser}  from '../controllers/users.js'
+
+//all routes in here are starting with /users
+router.get('/', getUsers);
+router.post('/', createUser);
+router.delete('/:id', deleteUser);
+router.get('/:id', getUserById); 
+router.patch('/:id', patchUser);
+
+export default router;
