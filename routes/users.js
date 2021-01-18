fc8f96617ea5bf1190cf13141d21e4785a1d6b0b
Mehuu26 <59767245+Mehuu26@users.noreply.github.com>
2021-01-18 19:47:04 +0100

    routing methods

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,13 @@ import express from 'express';
 
 const router = express.Router();
 
-import { getUsers}  from '../controllers/users.js'
+import { getUsers, createUser, deleteUser, getUserById, patchUser}  from '../controllers/users.js'
 
 //all routes in here are starting with /users
 router.get('/', getUsers);
+router.post('/', createUser);
+router.delete('/:id', deleteUser);
+router.get('/:id', getUserById); 
+router.patch('/:id', patchUser);
 
 export default router;
