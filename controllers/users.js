bc4d16b2edf21740199fd8fbe27ef16002034833
DESKTOP-27CQTIN\grzegurz <grzegorzstudzinski@hotmail.com>
2021-01-26 14:58:54 +0100

    singup & signin for users

--- controllers/users.js
+++ controllers/users.js
@@ -1,46 +1,57 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
 import User from '../models/user.js';
 
+export const signin = async (req,res) => {
+    const { email, password } = req.body;
+
+    try {
+        const existingUser = await User.findOne({ email });
+        if(!existingUser) return res.status(404).json({ message: "User does not exist."});
+        
+        const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
+        if(!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" });
+
+        const token = jwt.sign({ email: existingUser.email, id: existingUser._id}, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+        res.status(200).json({ result: existingUser, token });
+
+    } catch (error) {
+        res.status(500).json({message: 'Something went wrong'});
+    }
+
+};
+
+export const signup = async (req,res) => {
+    const { email, password, confirmPassword, firstName, lastName } = req.body;
+
+    try {
+        const existingUser = await User.findOne({ email });
+        if(existingUser) return res.status(400).json({ message: "User already exist."});
+
+        if(password !== confirmPassword)   return res.status(400).json({ message: "Password don't match."});
+
+        const hashedPassword = await bcrypt.hash(password, 12);
+
+        const result = await User.create({ email, password: hashedPassword, name: `${firstName} ${lastName}`})
+
+        const token = jwt.sign({ email: result.email, id: result._id}, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+        res.status(200).json({ result: result, token });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+};
+
 export const getUsers = async (req,res) => {
+
     try {
         const users = await User.find();
         res.status(200).json(users);
     } catch (error) {
         res.status(404).json({message:error.message});
     }
-}
 
-export const createUser = async (req,res) => {
-    const user = req.body;
-    const newUser = new User(user);
-    try {
-        await newUser.save();
-        res.status(201).json(newUser);
-
-    } catch (error) {
-        res.status(409).json({message :error.message})
-    }
-}
-
-export const deleteUser = async (req,res) => {
-    
-    User.remove({_id: req.params.id })
-        .then((resp) => res.status(200).json(resp))
-        .catch((err) => res.status(400).json("request failed"));
-
-}
-
-export const getUserById = async (req,res) => {
-
-    User.findById(req.params.id)
-        .then((resp) => res.status(200).json(resp))
-        .catch((err) => res.status(400).json("Request failed"));
-
-}
-
-export const patchUser = async (req,res) => {
-    
-    User.updateOne({_id: req.params.id} , {$set: req.body })
-        .then((resp) => res.status(200).json(resp))
-        .catch((err) => res.status(400).json("Request failed"));
-
-}
+};
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose'
 import cors from 'cors'
 import activitiesRoutes from './routes/activities.js';
+import userRoutes from './routes/users.js';
 
 const app = express();
 //const PORT = 5000;
@@ -13,6 +14,7 @@ app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
 app.use('/activities', activitiesRoutes);
+app.use('/users', userRoutes);
 
 //app.get('/',(req,res) => res.send('Hello from homepage'));
 //app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`));
