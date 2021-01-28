fd8d7a5220bb24f5f6f98cee095d45ea242a262a
DESKTOP-27CQTIN\grzegurz <grzegorzstudzinski@hotmail.com>
2021-01-28 01:12:38 +0100

    swagger documentation

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ import cors from 'cors'
 import activitiesRoutes from './routes/activities.js';
 import userRoutes from './routes/users.js';
 import morgan from 'morgan';
+import swaggerUi from 'swagger-ui-express';
+
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const swaggerDocument = require('./swagger.json');
 
 const app = express();
-//const PORT = 5000;
 
-//app.use(bodyParser.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api/v1', userRoutes, activitiesRoutes);
+
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
@@ -17,9 +23,6 @@ app.use(morgan('combined'));
 
 app.use('/activities', activitiesRoutes);
 app.use('/users', userRoutes);
-
-//app.get('/',(req,res) => res.send('Hello from homepage'));
-//app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`));
 
 const CONNECTION_URL = `mongodb+srv://user:${process.env.MONGO_PASSWORD}@cluster0.7soo3.mongodb.net/<dbname>?retryWrites=true&w=majority`;
 const PORT = process.env.PORT || 5000;
